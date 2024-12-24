3ef9285a118be36f31168d5e365f9c5bfd9c3d21
SoelMgd <soel.megdoud@gmail.com>
2024-12-24 19:49:33 +0100

    multigen_SD14

--- multigen/run_pipeline.sh
+++ multigen/run_pipeline.sh
@@ -7,13 +7,13 @@ NUM_GPUS=8
 MASTER_PORT=$(expr $RANDOM + 1000)
 
 # text config
-TEXT_FILE_PATH="ade20k_obj_comp_5_1k.json"
+TEXT_FILE_PATH="coco_obj_comp_5_1k.json" #"ade20k_obj_comp_5_1k.json"
 
 # model config
-MODEL_NAME="CompVis/stable-diffusion-v1-4"
+MODEL_NAME="mlpc-lab/TokenCompose_SD14_A" #"CompVis/stable-diffusion-v1-4"
 
 # output dir config
-OUTPUT_DIR_NAME="StableDiffusion1.4"
+OUTPUT_DIR_NAME= "TC_SD14_A" #"StableDiffusion1.4"
 SAMPLE_IMG_DIR="sample_imgs/${OUTPUT_DIR_NAME}"
 SAMPLE_JSON_DIR="sample_jsons/${OUTPUT_DIR_NAME}"
 mkdir -p $SAMPLE_IMG_DIR
