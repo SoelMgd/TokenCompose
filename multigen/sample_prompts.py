6e4d53839d64bc0c03a6b328286bddb1e185b9f6
SoelMgd <soel.megdoud@gmail.com>
2024-12-27 17:49:14 +0100

    prompts5

--- multigen/sample_prompts.py
+++ multigen/sample_prompts.py
@@ -4,7 +4,7 @@ with open("ade20k_obj_comp_5_1k.json", "r") as f:
     data = json.load(f)
 
 # Garder les 100 premiers prompts
-reduced_data = data[:25]
+reduced_data = data[:5]
 
-with open("ade20k_obj_comp_5_25.json", "w") as f:
+with open("ade20k_obj_comp_5_5.json", "w") as f:
     json.dump(reduced_data, f, indent=4)
