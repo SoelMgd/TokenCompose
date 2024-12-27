a60fd1364a4044363779bee7a390bf57c0f48318
SoelMgd <soel.megdoud@gmail.com>
2024-12-27 19:28:29 +0100

    test

--- train/src/vizualize_attn.py
+++ train/src/vizualize_attn.py
@@ -1,26 +1,25 @@
 import torch
 from diffusers import StableDiffusionPipeline
-from attn_utils import AttentionStore, register_attention_control, get_cross_attn_map_from_unet
-from transformers import CLIPTokenizer
 import matplotlib.pyplot as plt
+from attn_utils import AttentionStore, register_attention_control, get_cross_attn_map_from_unet
 import os
 
 # Charger le modèle depuis HuggingFace
-model_id = "mlpc-lab/TokenCompose_SD14_A"
+model_id = "mlpc-lab/TokenCompose_SD14_B"
 device = "cuda"
 
 # Initialiser AttentionStore pour capturer les cartes d'attention
 attention_store = AttentionStore()
 
-# Charger le pipeline
+# Charger la pipeline
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
 pipe.to(device)
 
 # Remplacer le U-Net dans la pipeline pour enregistrer les cartes d'attention
 register_attention_control(pipe.unet, attention_store)
 
-# Tokenizer pour identifier les indices des tokens
-tokenizer = CLIPTokenizer.from_pretrained(model_id)
+# Récupérer le tokenizer depuis la pipeline
+tokenizer = pipe.tokenizer
 
 # Prompt
 prompt = "A cat and a wine glass"
@@ -29,24 +28,23 @@ prompt = "A cat and a wine glass"
 image = pipe(prompt).images[0]
 
 # Sauvegarder l'image générée
-output_dir = "./viz_attn"
+output_dir = "./output"
 os.makedirs(output_dir, exist_ok=True)
 image.save(os.path.join(output_dir, "generated_image.png"))
 
 # Récupérer les cartes d'attention
 attn_maps = get_cross_attn_map_from_unet(attention_store, is_training_sd21=False)
 
-# Trouver l'index des tokens
-tokens = tokenizer(prompt, return_tensors="pt", padding="max_length", truncation=True)
-token_indices_cat = tokenizer.encode("cat")[1:-1]  # Indices pour "cat"
-token_indices_glass = tokenizer.encode("wine glass")[1:-1]  # Indices pour "wine glass"
+# Identifier les indices des tokens
+token_indices_cat = tokenizer.encode("cat")[1:-1]
+token_indices_glass = tokenizer.encode("wine glass")[1:-1]
 
 # Visualiser et sauvegarder les cartes d'attention
 for token_name, token_indices in [("cat", token_indices_cat), ("wine_glass", token_indices_glass)]:
-    # Moyenne des cartes d'attention pour toutes les têtes, pour le premier niveau "down_64"
+    # Moyenne des cartes d'attention pour toutes les têtes
     attention_map_token = attn_maps["down_64"][0][..., token_indices].mean(dim=-1).cpu().numpy()
 
-    # Sauvegarder la carte d'attention sous forme d'image
+    # Sauvegarder la carte d'attention
     plt.figure(figsize=(8, 8))
     plt.imshow(attention_map_token, cmap="viridis")
     plt.title(f"Cross-attention Map: {token_name}")
