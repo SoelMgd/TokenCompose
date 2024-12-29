00f0eda059f915969dd716b0ba070cadfe2651b7
SoelMgd <soel.megdoud@gmail.com>
2024-12-29 22:17:06 +0100

    test inference CLIP

--- multigen/test_CLIP.py
+++ multigen/test_CLIP.py
@@ -3,25 +3,7 @@ from torch import nn
 from diffusers import StableDiffusionPipeline
 from transformers import CLIPTextModel, CLIPTokenizer
 
-# Wrapper pour adapter les dimensions et conserver les méthodes d'origine
-class CLIPTextWrapper(nn.Module):
-    def __init__(self, text_encoder, target_dim):
-        super().__init__()
-        self.text_encoder = text_encoder
-        self.projection = nn.Linear(512, target_dim)  # Adapter de 512 à 768
 
-    def forward(self, input_ids, attention_mask=None):
-        outputs = self.text_encoder(input_ids=input_ids, attention_mask=attention_mask)
-        adapted_outputs = self.projection(outputs.last_hidden_state)
-        return adapted_outputs
-
-    def __getattr__(self, name):
-        # Protéger contre la récursion infinie
-        if name in self.__dict__:
-            return self.__dict__[name]
-        if hasattr(self.text_encoder, name):
-            return getattr(self.text_encoder, name)
-        raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
 
 # Charger la pipeline Stable Diffusion
 model_id = "mlpc-lab/TokenCompose_SD14_A"
@@ -30,15 +12,13 @@ device = "cuda"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
 
 # Charger le modèle CLIP avec une taille de sortie de 512
-new_clip_model_id = "openai/clip-vit-base-patch32"
+new_clip_model_id = "openai/clip-vit-base-patch16" #"openai/clip-vit-base-patch32"
 new_text_encoder = CLIPTextModel.from_pretrained(new_clip_model_id)
 new_tokenizer = CLIPTokenizer.from_pretrained(new_clip_model_id)
 
-# Ajouter une projection pour rendre les dimensions compatibles
-adapted_text_encoder = CLIPTextWrapper(new_text_encoder, target_dim=768)
 
 # Remplacer les composants CLIP dans la pipeline
-pipe.text_encoder = adapted_text_encoder
+pipe.text_encoder = new_text_encoder
 pipe.tokenizer = new_tokenizer
 
 # Transférer la pipeline sur le GPU
