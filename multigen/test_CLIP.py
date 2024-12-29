6abed0c30e7fb6738e3fc65cf613527ce8b10d8a
SoelMgd <soel.megdoud@gmail.com>
2024-12-29 20:08:51 +0100

    test inference CLIP

--- multigen/test_CLIP.py
+++ multigen/test_CLIP.py
@@ -2,13 +2,16 @@ import torch
 from diffusers import StableDiffusionPipeline
 from transformers import CLIPTextModel, CLIPTokenizer
 
-model_id = "mlpc-lab/TokenCompose_SD21_A"
+model_id = "mlpc-lab/TokenCompose_SD14_A"
 device = "cuda"
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
 
 # Charger un autre modèle CLIP
-new_clip_model_id = "openai/clip-vit-base-patch32"  # Ou votre modèle fine-tuné
+from transformers import CLIPTextModel, CLIPTokenizer
+
+# Charger un modèle CLIP compatible
+new_clip_model_id = "openai/clip-vit-base-patch32"  # Modèle compatible avec SD1.4
 new_text_encoder = CLIPTextModel.from_pretrained(new_clip_model_id)
 new_tokenizer = CLIPTokenizer.from_pretrained(new_clip_model_id)
 
@@ -19,6 +22,8 @@ pipe.tokenizer = new_tokenizer
 pipe = pipe.to(device)
 
 prompt = "A cat and a wine glass"
+
+
 image = pipe(prompt).images[0]  
 
 image.save('testing_image.png')
