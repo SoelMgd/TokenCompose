a09037cc6b9e20c6baf42c065b00cb2cb3f63d0d
SoelMgd <soel.megdoud@gmail.com>
2024-12-29 19:09:55 +0100

    test inference CLIP

--- multigen/test_CLIP.py
+++ multigen/test_CLIP.py
@@ -10,6 +10,9 @@ pipe = pipe.to(device)
 
 # Inspecter le modèle CLIP utilisé (encodeur texte)
 text_encoder = pipe.text_encoder
+vision_encoder = pipe.vae  # Ou essayez pipe.image_encoder si disponible
+print("Vision encoder: ", vision_encoder.config)
+
 
 # Afficher des informations sur l'architecture
 print(f"CLIP Text Encoder: {text_encoder.config.architectures}")
