abd0a577457cfd590e98bfa6dc79a057e79cc48e
SoelMgd <soel.megdoud@gmail.com>
2024-12-29 22:20:47 +0100

    test inference CLIP

--- multigen/test_CLIP.py
+++ multigen/test_CLIP.py
@@ -12,11 +12,11 @@ device = "cuda"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
 
 # Charger le modèle CLIP avec une taille de sortie de 512
-new_clip_model_id = "openai/clip-vit-base-patch16" #"openai/clip-vit-base-patch32"
+new_clip_model_id = "openai/clip-vit-large-patch14-336" #"openai/clip-vit-base-patch32"
 new_text_encoder = CLIPTextModel.from_pretrained(new_clip_model_id)
 new_tokenizer = CLIPTokenizer.from_pretrained(new_clip_model_id)
 
-
+print(new_clip_model_id)
 # Remplacer les composants CLIP dans la pipeline
 pipe.text_encoder = new_text_encoder
 pipe.tokenizer = new_tokenizer
