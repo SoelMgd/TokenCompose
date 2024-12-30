f947a9bf34759f95e842e2f5ed086c025ba27f68
SoelMgd <soel.megdoud@gmail.com>
2024-12-30 15:46:26 +0100

    CLIP_test_finetuned

--- multigen/test_CLIP.py
+++ multigen/test_CLIP.py
@@ -11,12 +11,17 @@ device = "cuda"
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
 
-# Charger le modèle CLIP avec une taille de sortie de 512
+'''
+# Charger le modèle CLIP
 new_clip_model_id = "openai/clip-vit-large-patch14-336" #"openai/clip-vit-base-patch32"
 new_text_encoder = CLIPTextModel.from_pretrained(new_clip_model_id)
-new_tokenizer = CLIPTokenizer.from_pretrained(new_clip_model_id)
+new_tokenizer = CLIPTokenizer.from_pretrained(new_clip_model_id)'''
 
-print(new_clip_model_id)
+fine_tuned_clip_model_dir = "../train/clip_finetuned_model"  # Chemin où vos poids sont sauvegardés
+new_text_encoder = CLIPTextModel.from_pretrained(fine_tuned_clip_model_dir)
+new_tokenizer = CLIPTokenizer.from_pretrained(fine_tuned_clip_model_dir)
+
+
 # Remplacer les composants CLIP dans la pipeline
 pipe.text_encoder = new_text_encoder
 pipe.tokenizer = new_tokenizer
