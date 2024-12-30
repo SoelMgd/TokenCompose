b4a3f72f62349860eab6ad5646e498e288e08a06
SoelMgd <soel.megdoud@gmail.com>
2024-12-30 16:09:46 +0100

    coco_50

--- multigen/sample_prompts.py
+++ multigen/sample_prompts.py
@@ -4,7 +4,7 @@ with open("coco_obj_comp_5_1k.json", "r") as f:
     data = json.load(f)
 
 # Garder les 100 premiers prompts
-reduced_data = data[:25]
+reduced_data = data[:50]
 
-with open("coco_obj_comp_5_25.json", "w") as f:
+with open("coco_obj_comp_5_50.json", "w") as f:
     json.dump(reduced_data, f, indent=4)
