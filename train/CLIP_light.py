bd609fc591ad26e295d572d5a145666dd6e9f0d9
SoelMgd <soel.megdoud@gmail.com>
2024-12-30 14:18:39 +0100

    CLIP_light

--- train/CLIP_light.py
+++ train/CLIP_light.py
@@ -260,8 +260,8 @@ if __name__ == "__main__":
 
 
     transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.ToTensor()
+        #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
     print("dataset")
     dataset = CocoGsamDataset(img_dir=img_dir, seg_dir=seg_dir, transform=transform)
