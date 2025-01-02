672116119819ef60c2e092ba266238d1aafe48e7
SoelMgd <soel.megdoud@gmail.com>
2025-01-02 17:12:53 +0100

    get visualisations

--- train/src/vizualize_attn.py
+++ train/src/vizualize_attn.py
@@ -47,6 +47,18 @@ model_id = "CompVis/stable-diffusion-v1-4" #"mlpc-lab/TokenCompose_SD14_B"
 device = "cuda"
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
+
+from transformers import CLIPTextModel, CLIPTokenizer
+
+fine_tuned_clip_model_dir = "../clip_finetuned_model"  # Chemin où vos poids sont sauvegardés
+new_text_encoder = CLIPTextModel.from_pretrained(fine_tuned_clip_model_dir)
+new_tokenizer = CLIPTokenizer.from_pretrained(fine_tuned_clip_model_dir)
+
+
+# Remplacer les composants CLIP dans la pipeline
+pipe.text_encoder = new_text_encoder
+pipe.tokenizer = new_tokenizer
+
 pipe.to(device)
 print("Pipeline chargée avec succès.")
 
